482e75fa2993e7dcbf65711c749e2696795bb975
Matt Williams <williams.e.matt@gmail.com>
2015-10-04 21:17:59 -0500

    Add a settings page; Update readme with gif mvp

---
+++ tip-calculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tip-calculator
+//
+//  Created by Matthew Williams on 9/29/15.
+//  Copyright (c) 2015 MattWilliams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
